2263546501393e0d40e6893eac698cd393f9aadf
Will Studabaker <will.studabaker@gmail.com>
2020-11-23 13:10:55 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,100 @@
+INSERT INTO users (name, age) VALUES ("Bob", 47);
+INSERT INTO users (name, age) VALUES ("Kate", 28);
+INSERT INTO users (name, age) VALUES ("Dan", 24);
+INSERT INTO users (name, age) VALUES ("Edna", 87);
+INSERT INTO users (name, age) VALUES ("Tina", 44);
+INSERT INTO users (name, age) VALUES ("Lois", 21);
+INSERT INTO users (name, age) VALUES ("Tom", 49);
+INSERT INTO users (name, age) VALUES ("Ethan", 50);
+INSERT INTO users (name, age) VALUES ("Yan", 26);
+INSERT INTO users (name, age) VALUES ("Laura", 34);
+INSERT INTO users (name, age) VALUES ("Rich", 44);
+INSERT INTO users (name, age) VALUES ("Sandy", 76);
+INSERT INTO users (name, age) VALUES ("Peter", 45);
+INSERT INTO users (name, age) VALUES ("Ivan", 43);
+INSERT INTO users (name, age) VALUES ("Miguel", 23);
+INSERT INTO users (name, age) VALUES ("Sara", 52);
+INSERT INTO users (name, age) VALUES ("Walter", 57);
+INSERT INTO users (name, age) VALUES ("Trish", 63);
+INSERT INTO users (name, age) VALUES ("Anthony", 45);
+INSERT INTO users (name, age) VALUES ("Norm", 32);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+Values ("Colonize Mars", "Board Game", 20000, "January 2020", "December 2020");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+Values ("Spirit Haven", "Board Game", 15000, "October 2019", "March 2021");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+Values ("Seventh Staircase", "Graphic Novel", 7000, "July 2020", "November 2021");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+Values ("Myth Hunters", "TV Show", 45000, "September 2019", "June 2020");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+Values ("Under the Floorboards", "Board Game", 17500, "February 2018", "April 2021");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+Values ("Nexus", "Graphic Novel", 13590, "June 2019", "March 2020");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+Values ("Lens Flare", "TV Show", 90400, "December 2020", "August 2021");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+Values ("Doldrums", "Board Game", 6500, "July 2019", "January 2020");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+Values ("Hidden Transmissions", "Short Film", 18700, "Novemeber 2020", "June 2021");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+Values ("Pact", "Graphic Novel", 3300, "March 2018", "March 2019");
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (200, 5, 1);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (400, 7, 10);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (1000, 1, 2);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (750, 17, 6);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (1200, 4, 19);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (25, 9, 20);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (820, 11, 2);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (450, 4, 3);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (1100, 15, 7);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (200, 12, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (740, 10, 5);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (650, 8, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (475, 7, 7);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (325, 2, 3);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (500, 16, 1);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (750, 14, 6);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (900, 20, 10);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (1500, 19, 2);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (1300, 18, 4);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (275, 7, 3);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (950, 6, 5);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (400, 13, 3);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (875, 4, 2);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (100, 11, 10);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (275, 6, 8);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (1200, 3, 10);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (140, 17, 1);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (600, 18, 5);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (725, 14, 6);
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (700, 4, 8);
